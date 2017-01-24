c2b5346d42c2400a444f04c47eda45361785f59e
Sigurd Holsen <sighol@gmail.com>
2017-01-24 20:48:42 +0100

    failed attempt with termcolor and walkdir

--- src/main.rs
+++ src/main.rs
@@ -5,38 +5,89 @@ extern crate termcolor;
 use std::io;
 use std::io::Write;
 
-use walkdir::{WalkDir, DirEntry};
+use walkdir::{WalkDir, DirEntry, WalkDirIterator};
 use termcolor::{Color, ColorChoice, ColorSpec, Stdout, WriteColor};
 
-fn main() {
-    let walker = WalkDir::new(".");
-    let walker = walker.sort_by(|x, y| x.cmp(y));
-    let walker = walker.into_iter().filter_map(|e| e.ok());
-    for entry in walker {
-        let mut spaces = String::new();
-        for _ in 0..entry.depth() * 2 {
-            spaces.push('-')
-        }
+enum DirSign {
+    Cross,
+    Vert,
+    Horz,
+    LastFile,
+}
 
-        if let Some(name) = get_name(&entry) {
-            print!("{}", spaces);
-            let mut color = None;
-            if let Ok(metadata) = entry.path().metadata() {
-                if metadata.is_dir() {
-                    color = Some(Color::Blue);
-                }
-            }
-
-            write(color, &name);
-            println!("");
-        }
+impl DirSign {
+    fn char(self) -> char {
+        return match self {
+            DirSign::Horz => '─',
+            DirSign::Cross => '├',
+            DirSign::Vert => '|',
+            DirSign::LastFile => '└',
+        };
     }
 }
 
-fn write(color: Option<Color>, text: &str) -> io::Result<()> {
+/// ├── _config.yml
+/// ├── _drafts
+/// |   ├── begin-with-the-crazy-ideas.textile
+/// |   └── on-simplicity-in-technology.markdown
+/// ├── _includes
+/// |   ├── footer.html
+/// |   └── header.html
+/// ├── _layouts
+/// |   ├── default.html
+/// |   └── post.html
+/// ├── _posts
+/// |   ├── 2007-10-29-why-every-programmer-should-play-nethack.textile
+/// |   └── 2009-04-26-barcamp-boston-4-roundup.textile
+/// ├── _data
+/// |   └── members.yml
+/// ├── _site
+/// └── index.html
+
+fn is_hidden(entry: &DirEntry) -> bool {
+    entry.file_name()
+        .to_str()
+        .map(|s| s.starts_with("."))
+        .unwrap_or(false)
+}
+
+fn main() {
     let mut stdout = Stdout::new(ColorChoice::Always);
-    try!(stdout.set_color(ColorSpec::new().set_fg(color)));
-    write!(&mut stdout, "{}", text)
+
+    let walker = WalkDir::new(".");
+    let walker = walker.sort_by(|x, y| x.cmp(y));
+    let walker = walker.into_iter();
+        // .filter_entry(|e| !is_hidden(e));
+        // .filter_map(|e| e.ok());
+
+
+
+    for entry in walker {
+        if entry.is_err() { continue; }
+
+        let entry = entry.unwrap();
+
+        let mut prefix = String::new();
+        for _ in 0..entry.depth() {
+            prefix.push_str("|   ")
+        }
+
+        if let Some(name) = get_name(&entry) {
+            let mut color = None;
+            if let Ok(metadata) = entry.path().metadata() {
+                if metadata.is_dir() {
+                    color = Some(Color::Magenta);
+                }
+            }
+
+            prefix.push(DirSign::Cross.char());
+            prefix.push(DirSign::Horz.char());
+            prefix.push(DirSign::Horz.char());
+            print!("{} ", prefix);
+            write(&mut stdout, color, &name);
+            println!("");
+        }
+    }
 }
 
 fn get_name(entry: &DirEntry) -> Option<String> {
